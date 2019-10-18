785c6decc51aa48b720238a9e759a6b8e0a38cef
Nexwich <alterfall@mail.ru>
2019-10-18 16:50:37 +0300

    Исправил ошибку мешающую вписать свое значения для полей «Email отправителя» и «Email для ответа»

--- src/netcat/modules/notice/admin/views/message/add.view.php
+++ src/netcat/modules/notice/admin/views/message/add.view.php
@@ -1,4 +1,4 @@
-<?php if(!class_exists('nc_core')){
+<?php if (!class_exists('nc_core')) {
   die;
 }
 
@@ -28,7 +28,7 @@ get_style_and_script(array("selectize", "ckeditor"));
         <div class="form">
           <form enctype='multipart/form-data' method='post' action='<?= $MODULE_PATH ?>admin/?controller=message&action=save'>
             <div>
-              <? if(!empty($message)){ ?>
+              <? if (!empty($message)) { ?>
                 <input type='hidden' name='f_Notice_Message_ID' value='<?= $message['id'] ?>'>
               <? } ?>
               <input type='hidden' name='f_Notice_Rule_ID' value='<?= $rule['id'] ?>'>
@@ -44,11 +44,11 @@ get_style_and_script(array("selectize", "ckeditor"));
             </div>
             <div class="nc-form-row">
               <label for="f_Email_From"><?= OCTOCORP_MODULE_NOTICE_MESSAGE_EMAIL_FROM ?></label>
-              <select id="f_Email_From" name='f_Email_From' class="js-selectize" data-select='{"persist":false, "create": true}' data-options='{"load":{"type":"user", "value":["<?= $message['email_from'] ?>"]}, "essence":"user"}' required='required' <? if($nc_core->get_settings('SpamUseTransport', 'system') == 'Smtp'){ ?> disabled="disabled"<? } ?>></select>
+              <select id="f_Email_From" name='f_Email_From' class="js-selectize" data-select='{"persist":false, "create": true}' data-options='{"load":{"type":"user", "value":<? if ($message['email_from']) { ?>["<?= $message['email_from'] ?>"]<? }else { ?>""<? } ?>}, "essence":"user"}' required='required' <? if ($nc_core->get_settings('SpamUseTransport', 'system') == 'Smtp') { ?> disabled="disabled"<? } ?>></select>
             </div>
             <div class="nc-form-row">
               <label for="f_Email_Reply"><?= OCTOCORP_MODULE_NOTICE_MESSAGE_EMAIL_REPLY ?></label>
-              <select id="f_Email_Reply" name='f_Email_Reply' class="js-selectize" data-select='{"persist":false, "create": true}' data-options='{"load":{"type":"user", "value":["<?= $message['email_reply'] ?>"]}, "essence":"user"}' required='required' <? if($nc_core->get_settings('SpamUseTransport', 'system') == 'Smtp'){ ?> disabled="disabled"<? } ?>></select>
+              <select id="f_Email_Reply" name='f_Email_Reply' class="js-selectize" data-select='{"persist":false, "create": true}' data-options='{"load":{"type":"user", "value":<? if ($message['email_reply']) { ?>["<?= $message['email_reply'] ?>"]<? }else { ?>""<? } ?>}, "essence":"user"}' required='required' <? if ($nc_core->get_settings('SpamUseTransport', 'system') == 'Smtp') { ?> disabled="disabled"<? } ?>></select>
             </div>
             <div class="nc-form-row">
               <label><?= OCTOCORP_MODULE_NOTICE_MESSAGE_NAME_FROM ?></label>
@@ -61,7 +61,7 @@ get_style_and_script(array("selectize", "ckeditor"));
             <div class="nc-form-row">
               <label for="f_Message"><?= OCTOCORP_MODULE_NOTICE_MESSAGE_MESSAGE ?></label>
               <textarea id='f_Message' name='f_Message' cols="30" rows="5" class="no_cm"><?= $message['message'] ?></textarea>
-              <script type='text/javascript'>nc_notice_editor('f_Message')</script>
+              <script type='text/javascript'>nc_notice_editor('f_Message');</script>
             </div>
           </form>
         </div>
