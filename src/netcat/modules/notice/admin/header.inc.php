1566b8aa8dce290613ba67e270f6a1bac0f93350
Nexwich <alterfall@mail.ru>
2020-11-23 19:22:59 +0300

    исправление для неткат 6

--- src/netcat/modules/notice/admin/header.inc.php
+++ src/netcat/modules/notice/admin/header.inc.php
@@ -1,8 +1,12 @@
 <?
 
-$NETCAT_FOLDER = realpath(dirname(__FILE__) . "/../../../../") . "/";
-include_once $NETCAT_FOLDER . "vars.inc.php";
-require_once __DIR__ . "/function.inc.php";
+$NETCAT_FOLDER = join(strstr(__FILE__, "/") ? "/" : "\\", array_slice(preg_split("/[\/\\\]+/", __FILE__), 0, -5)) . (strstr(__FILE__, "/") ? "/" : "\\");
+
+require_once $NETCAT_FOLDER . "vars.inc.php";
+require $INCLUDE_FOLDER . "index.php";
+require_once $ADMIN_FOLDER . "function.inc.php";
+require_once $ADMIN_FOLDER . "modules/ui.php";
+require_once $MODULE_FOLDER . "/notice/function.inc.php";
 
 /** @var Permission $perm */
 $perm->ExitIfNotAccess(NC_PERM_MODULE, 0, 0, 0, 1);
--- src/netcat/modules/notice/admin/no_header.inc.php
+++ src/netcat/modules/notice/admin/no_header.inc.php
@@ -1,13 +1,11 @@
 <?php
+$NETCAT_FOLDER = join(strstr(__FILE__, "/") ? "/" : "\\", array_slice(preg_split("/[\/\\\]+/", __FILE__), 0, -5)) . (strstr(__FILE__, "/") ? "/" : "\\");
 
-/**
- * Script that can be included to initialize NetCat scripts and check user rights
- * (e.g. in scripts that should output JSON data)
- */
-
-$NETCAT_FOLDER = realpath(dirname(__FILE__) . "/../../../../") . "/";
-include_once $NETCAT_FOLDER . "vars.inc.php";
-require_once __DIR__ . "/function.inc.php";
+require_once $NETCAT_FOLDER . "vars.inc.php";
+require $INCLUDE_FOLDER . "index.php";
+require_once $ADMIN_FOLDER . "function.inc.php";
+require_once $ADMIN_FOLDER . "modules/ui.php";
+require_once $MODULE_FOLDER . "/notice/function.inc.php";
 
 /** @var Permission $perm */
 $perm->ExitIfNotAccess(NC_PERM_MODULE, 0, 0, 0, 1);
